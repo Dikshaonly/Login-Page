bc8df3cfcfb1f8ca24f6efcbdf91b42a2f3ceb5f
Dikshaonly <pradhandiksha2004@gmail.com>
2025-11-18 10:25:36 +0545

    Added Identity

--- Program.cs
+++ Program.cs
@@ -14,9 +14,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddIdentity<Users, IdentityRole>(options=>
 {
     options.Password.RequireNonAlphanumeric = false;
-}
-);
-
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequiredLength = 8;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail =false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+})
+ .AddEntityFrameworkStores<AppDbContext>()
+ .AddDefaultTokenProviders();
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
