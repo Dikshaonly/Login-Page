70f9313b58afbcc8fbfe34445687af3bb411c34c
Dikshaonly <pradhandiksha2004@gmail.com>
2025-11-18 21:15:32 +0545

    add model for register view

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -5,9 +5,14 @@ namespace loginPage.ViewModels{
         [Required(ErrorMessage = "Name is Required!")]
         public string Name{get;set;}
 
-        [Required()]
+        [Required(ErrorMessage = "Email is Required!")]
+        [EmailAddress]
         public string Email{get;set;}
+
+        [Required(ErrorMessage = "Password is Required!")]
+        [DataType(DataType.Password)]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
         public string Password{get;set;}
-        public string ConfirmedPassword{get;set;}
+
     }
 }
