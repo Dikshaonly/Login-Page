f2e0966dea03f93aa8b71f696d9ac6150b30179a
Dikshaonly <pradhandiksha2004@gmail.com>
2025-11-20 12:10:14 +0545

    edited HomePage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using loginPage.Models;
 
@@ -7,14 +8,22 @@ namespace loginPage.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<Users> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<Users> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        if(User.Identity.IsAuthenticated){
+            var user=await _userManager.GetUserAsync(User);
+            ViewBag.FullName=user.FullName;
+            ViewBag.Email=user.Email;
+        }
+        
         return View();
     }
 
